53d2e5a7009cc3ebd7365f0aeb64ff1a87f4a782
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-06-17 19:49:07 +0900

    🚨 エラーを修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,91 +1,12 @@
 import { css } from '@emotion/react';
 import { Header, Container, Group, rem, useMantineTheme } from '@mantine/core';
 
-import { useDisclosure } from '@mantine/hooks';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import type { FC, ReactNode } from 'react';
 import { pagesPath } from '@/lib/$path';
 
 const HEADER_HEIGHT = rem(60);
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dropdown: {
-    position: 'absolute',
-    top: HEADER_HEIGHT,
-    left: 0,
-    right: 0,
-    zIndex: 0,
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopWidth: 0,
-    overflow: 'hidden',
-
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '100%',
-  },
-
-  links: {
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  link: {
-    display: 'block',
-    lineHeight: 1,
-    padding: `${rem(8)} ${rem(12)}`,
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    },
-
-    [theme.fn.smallerThan('sm')]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
-    },
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({
-        variant: 'light',
-        color: theme.primaryColor,
-      }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-        .color,
-    },
-  },
-}));
 
 const links = [
   { link: '/dashboard', label: 'DashBoard' },
@@ -151,25 +72,17 @@ const HeaderLink: FC<HeaderLinkProps> = ({ href, children, active }) => {
 };
 
 export const HeaderResponsive: FC = () => {
-  const [opened, { toggle, close }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0]?.link);
-  const { classes, cx } = useStyles();
+  const { pathname } = useRouter();
+  const theme = useMantineTheme();
 
   const items = links.map((link) => (
-    <Link
+    <HeaderLink
       key={link.label}
       href={link.link}
-      className={cx(classes.link, {
-        [classes.linkActive]: active === link.link,
-      })}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-        close();
-      }}
+      active={link.link === pathname}
     >
       {link.label}
-    </Link>
+    </HeaderLink>
   ));
 
   return (
@@ -219,4 +132,4 @@ export const HeaderResponsive: FC = () => {
       </Container>
     </Header>
   );
-}
+};
