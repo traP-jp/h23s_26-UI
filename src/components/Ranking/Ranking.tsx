811fa140f13ebee8656df5641374719a94839c12
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-06-18 05:13:23 +0900

    ランキングの表示をいい感じに

--- src/components/Ranking/Ranking.tsx
+++ src/components/Ranking/Ranking.tsx
@@ -1,41 +1,95 @@
 import { css } from '@emotion/react';
+import { Center, Loader, useMantineTheme, Text } from '@mantine/core';
+import { IconCrown } from '@tabler/icons-react';
 import useSWR from 'swr';
 import { fetcher } from '@/lib/fetcher';
 import { getApiBaseUrl } from '@/lib/getApiBaseUrl';
-import type * as schema from '@/schema/schema';
+import type { GetRankingResponse } from '@/schema/schema';
 
 export function Ranking() {
-  const { data } = useSWR<schema.GetRankingResponse>(
+  const theme = useMantineTheme();
+  const { data } = useSWR<GetRankingResponse>(
     `${getApiBaseUrl()}/ranking`,
     fetcher,
   );
-  if (data === undefined) {
-    return <div>loading...</div>;
-  } else {
-    return (
-      <div>
-        <h1
+
+  const getRankColor = (rank: number) =>
+    rank === 1
+      ? theme.colors.yellow[7]
+      : rank === 2
+      ? theme.colors.gray[7]
+      : rank === 3
+      ? theme.colors.orange[7]
+      : theme.colors.gray[5];
+
+  const getRankIndex = (rank: number) =>
+    [1, 2, 3].includes(rank) ? (
+      <IconCrown size="1.5rem" fill="currentColor" />
+    ) : (
+      rank
+    );
+
+  return (
+    <div>
+      <h1
+        css={css`
+          padding: 0.25rem 1rem;
+          border-bottom: 1px solid ${theme.colors.gray[2]};
+          font-size: 1.5rem;
+          line-height: 2rem;
+        `}
+      >
+        ランキング
+      </h1>
+      {data ? (
+        <div
           css={css`
-            height: 4rem;
-            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 1rem;
+            gap: 16px;
           `}
         >
-          ランキング
-        </h1>
-        <div>
           {data.ranking.map((rank, index) => (
-            <div key={rank}>
+            <div
+              key={rank}
+              css={css`
+                display: flex;
+                width: min(400px, calc(100vw - 32px));
+                height: 2.5rem;
+                align-items: center;
+                border-radius: ${theme.radius.sm};
+                margin: 0 auto;
+                background-color: ${theme.fn.rgba(
+                  getRankColor(index + 1),
+                  0.2,
+                )};
+                gap: 16px;
+              `}
+            >
               <div
                 css={css`
+                  display: grid;
+                  width: 3rem;
+                  color: ${getRankColor(index + 1)};
+                  font-size: 1.5rem;
+                  font-weight: 500;
+                  place-content: center;
                   text-align: center;
                 `}
               >
-                {index + 1} {rank}
+                {getRankIndex(index + 1)}
               </div>
+              <Text size="lg">{rank}</Text>
             </div>
           ))}
         </div>
-      </div>
-    );
-  }
+      ) : (
+        <Center p="xl">
+          <Loader variant="oval" />
+        </Center>
+      )}
+    </div>
+  );
 }
