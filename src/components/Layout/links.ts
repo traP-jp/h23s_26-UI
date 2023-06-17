8eb2daf0062dfe14b3a55746288cb10fd34bace6
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-06-18 05:16:21 +0900

    修正

--- src/components/Layout/links.ts
+++ src/components/Layout/links.ts
@@ -3,5 +3,5 @@ import { IconCrown, IconHome, IconTargetArrow } from '@tabler/icons-react';
 export const links = [
   { Icon: IconHome, link: '/dashboard', label: 'DashBoard' },
   { Icon: IconTargetArrow, link: '/missions', label: 'Missions' },
-  { Icon: IconCrown, link: 'ranking', label: 'Ranking' },
+  { Icon: IconCrown, link: '/ranking', label: 'Ranking' },
 ] as const;
