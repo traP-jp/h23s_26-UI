c3bc8a28b7d66e353cee2c0cc061da0e91954e74
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-06-18 04:27:46 +0900

    データのローディング状態を上手くハンドリング + スタイリング

--- src/pages/missions.tsx
+++ src/pages/missions.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { Card, Overlay, Text, useMantineTheme } from '@mantine/core';
+import { Card, Overlay, Skeleton, Text, useMantineTheme } from '@mantine/core';
 import { IconCheck } from '@tabler/icons-react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
@@ -12,23 +12,28 @@ import type { GetMissionsResponse, GetUserResponse } from '@/schema/schema';
 const Missions: NextPage = () => {
   const theme = useMantineTheme();
 
-  const {
-    data: missions,
-    error,
-    isLoading,
-  } = useSWR<GetMissionsResponse>(`${getApiBaseUrl()}/missions`, fetcher);
+  const { data: missions } = useSWR<GetMissionsResponse>(
+    `${getApiBaseUrl()}/missions`,
+    fetcher,
+  );
   const { data: user } = useSWR<GetUserResponse>(
     `${getApiBaseUrl()}/users/me`,
     fetcher,
   );
 
-  if (error) return <div>failed to load: {error.message}</div>;
-  if (isLoading) return <div>loading...</div>;
-
   return (
     <Layout>
       <div>
-        <h1>Missions</h1>
+        <h1
+          css={css`
+            padding: 0.25rem 1rem;
+            border-bottom: 1px solid ${theme.colors.gray[2]};
+            font-size: 1.5rem;
+            line-height: 2rem;
+          `}
+        >
+          Missions
+        </h1>
         <div
           css={css`
             display: grid;
@@ -37,48 +42,50 @@ const Missions: NextPage = () => {
             grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
           `}
         >
-          {missions?.map((mission) => (
-            <Link
-              href={`/missions/${mission.id}`}
-              key={mission.id}
-              css={css`
-                color: inherit;
-                text-decoration: none;
-              `}
-            >
-              <Card
-                shadow="xs"
-                padding="lg"
-                css={css`
-                  transition: all 0.2s;
-
-                  &:hover {
-                    box-shadow: ${theme.shadows.sm};
-                    opacity: 0.8;
-                  }
-                `}
-              >
-                {user?.achieves.includes(mission.id) && (
-                  <Overlay
+          {missions
+            ? missions.map((mission) => (
+                <Link
+                  href={`/missions/${mission.id}`}
+                  key={mission.id}
+                  css={css`
+                    color: inherit;
+                    text-decoration: none;
+                  `}
+                >
+                  <Card
+                    shadow="xs"
+                    padding="lg"
                     css={css`
-                      display: grid;
-                      background-color: ${theme.fn.rgba(theme.white, 0.8)};
-                      color: ${theme.colors.lime[5]};
-                      place-items: center;
+                      transition: all 0.2s;
+
+                      &:hover {
+                        box-shadow: ${theme.shadows.sm};
+                        opacity: 0.8;
+                      }
                     `}
                   >
-                    <IconCheck size="5rem" />
-                  </Overlay>
-                )}
-                <Text fw="500" className="mission-title">
-                  {mission.name}
-                </Text>
-                <Text color="dimmed" className="mission-description">
-                  {mission.description}
-                </Text>
-              </Card>
-            </Link>
-          ))}
+                    {user?.achieves.includes(mission.id) && (
+                      <Overlay
+                        css={css`
+                          display: grid;
+                          background-color: ${theme.fn.rgba(theme.white, 0.8)};
+                          color: ${theme.colors.lime[5]};
+                          place-items: center;
+                        `}
+                      >
+                        <IconCheck size="5rem" />
+                      </Overlay>
+                    )}
+                    <Text fw="500" className="mission-title">
+                      {mission.name}
+                    </Text>
+                    <Text color="dimmed" className="mission-description">
+                      {mission.description}
+                    </Text>
+                  </Card>
+                </Link>
+              ))
+            : new Array(4).fill(0).map((_, i) => <Skeleton h={90} key={i} />)}
         </div>
       </div>
     </Layout>
