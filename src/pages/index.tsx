94c714f019418fbe87a8170b53592ece26dcd7f8
Kuryu <marikahoshina0205@gmail.com>
2023-06-17 11:30:11 +0900

    indexにHeaderを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { css } from '@emotion/react';
 import type { NextPage } from 'next';
 import { Description } from '@/components/Description';
+import { HeaderResponsive } from '@/components/Header';
 
 const Home: NextPage = () => {
   return (
     <>
+      <HeaderResponsive links={[]} />
       <Description
         title="Next.js with Mantine UI Template"
         description="Next.jsをMantine UIと一緒に色々セットアップしてあるオレオレテンプレートリポジトリです。ご利用は計画的に。"
