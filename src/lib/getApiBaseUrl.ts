e2b8b361c1c34e27c5705bee83aacd26a8734e4c
ras0q <ras0q@users.noreply.github.com>
2023-06-18 22:58:20 +0900

    Revert ":recycle: change url"
    This reverts commit c3acbd59bd2267f5e2eb52c0d0116ef79b21f5ec.

--- src/lib/getApiBaseUrl.ts
+++ src/lib/getApiBaseUrl.ts
@@ -1,5 +1,3 @@
 export const getApiBaseUrl = () => {
-  return (
-    process.env.NEXT_PUBLIC_API_BASE_URL ?? 'https://mission.trap.games/api/v1'
-  );
+  return process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:3000/api';
 };
